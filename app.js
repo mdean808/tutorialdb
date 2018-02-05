14e53c0b034c5f16ec122c36b7446a39f74db27d
Unicorn <unicorncalcs@gmail.com>
2018-02-05 12:07:59 -1000

    fixed stuff

--- app.js
+++ app.js
@@ -1,36 +1,33 @@
-var express = require('express');
-var path = require('path');
-var exphbs = require('express-handlebars');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var Recaptcha = require('recaptcha-verify');
-var swearjar = require('swearjar');
-var mysql = require('mysql');
-var GoogleAuth = require('google-auth-library');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const Recaptcha = require('recaptcha-verify');
+const swearjar = require('swearjar');
+const mysql = require('mysql');
+const GoogleAuth = require('google-auth-library');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+const index = require('./routes/index');
+const users = require('./routes/users');
 
-var app = express();
-var auth = new GoogleAuth;
-var client = new auth.OAuth2('513403102947-k7qrh4q7s7p5ar7b1dpo27o768e6iq8i.apps.googleusercontent.com', '', '');
-var morgan = '101672782678779786564';
-var username = '', title = '', link = '', desc = '', summary = '';
-var recaptcha = new Recaptcha({
+const app = express();
+const auth = new GoogleAuth;
+const client = new auth.OAuth2('513403102947-k7qrh4q7s7p5ar7b1dpo27o768e6iq8i.apps.googleusercontent.com', '', '');
+const morgan = '101672782678779786564';
+let username = '', title = '', link = '', desc = '', summary = '';
+const recaptcha = new Recaptcha({
 	secret: '6Lf1RzQUAAAAAIr_f2u4A2xbhlAYEPi8IJrZP7SD',
 	verbose: true
 });
+let con;
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -40,7 +37,7 @@ app.use('/users', users);
 
 app.get('/submit-captcha', function (req, res) {
 	// get the user response (from reCAPTCHA)
-	var userResponse = req.query['g-recaptcha-response'];
+	const userResponse = req.query['g-recaptcha-response'];
 	username = req.query.username;
 	title = req.query.title;
 	link = req.query.link;
@@ -51,12 +48,12 @@ app.get('/submit-captcha', function (req, res) {
 			console.log(error);
 		}
 		if (response.success) {
-			var allWords = title.concat(desc, ' ', link, ' ', username, ' ', summary);
+			const allWords = title.concat(desc, ' ', link, ' ', username, ' ', summary);
 			if (swearjar.profane(allWords)) {
 				res.status(200).redirect('/tutorial-submit?error=profanity');
 			} else {
 				res.status(200).redirect('/submitted');
-				var sql = "INSERT INTO tutorials (title, link, description, summary, author) VALUES ('" + title + "', '" + link + "', '" + desc + "', '" + summary + "', '" + username + "')";
+				const sql = "INSERT INTO tutorials (title, link, description, summary, author) VALUES ('" + title + "', '" + link + "', '" + desc + "', '" + summary + "', '" + username + "')";
 				con.query(sql, function (err, result) {
 					if (err) {
 						console.log(err);
@@ -93,7 +90,7 @@ function handleDisconnect() {
 	con.on('error', function (err) {
 		if (err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
 			handleDisconnect();                         // lost due to either server restart, or a
-		} else {                                      // connnection idle timeout (the wait_timeout
+		} else {                                      // connection idle timeout (the wait_timeout
 			console.log(err); // server variable configures this)
 		}
 	});
@@ -106,6 +103,7 @@ handleDisconnect();
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+/* Create DB (used locally)
 app.post('/api/create-db', function (req, res) {
 	con.query("CREATE DATABASE tutorialdb", function (err, result) {
 		if (err) {
@@ -116,9 +114,11 @@ app.post('/api/create-db', function (req, res) {
 		}
 	});
 });
+*/
 
+/* Crate table (used locally)
 app.get('/api/create-table', function (req, res) {
-	var sql = "CREATE TABLE tutorials (id INT AUTO_INCREMENT PRIMARY KEY, title VARCHAR(255), link VARCHAR(255), description VARCHAR(255), summary VARCHAR(255), author VARCHAR(255))";
+	const sql = "CREATE TABLE tutorials (id INT AUTO_INCREMENT PRIMARY KEY, title VARCHAR(255), link VARCHAR(255), description VARCHAR(255), summary VARCHAR(255), author VARCHAR(255))";
 	con.query(sql, function (err, result) {
 		if (err) {
 			console.log(err);
@@ -127,29 +127,34 @@ app.get('/api/create-table', function (req, res) {
 		}
 	});
 });
-
+<*/
 app.post('/api/add-tutorial', function (req, res) {
+	/** @namespace req.query.author */
+	const sql = "INSERT INTO tutorials (title, link, description, summary, author) VALUES ('" + req.query.title + "', '" + req.query.link + "', '" + req.query.desc + "', '" + req.query.summary + "', '" + req.query.author + "')";
 	if (con.state !== 'disconnected')
-		var sql = "INSERT INTO tutorials (title, link, description, summary, author) VALUES ('" + req.query.title + "', '" + req.query.link + "', '" + req.query.desc + "', '" + req.query.summary + "', '" + req.query.author + "')";
-	con.query(sql, function (err, result) {
-		if (err) {
-			console.log(err);
-		} else {
-			console.log("Added Tutorial:", result);
-			res.writeHead(200, {'Content-Type': 'application/json'});
-			res.end(JSON.stringify({
-				addedTutorial: result
-			}));
-		}
-	});
+		con.query(sql, function (err, result) {
+			if (err) {
+				console.log(err);
+			} else {
+				console.log("Added Tutorial:", result);
+				res.writeHead(200, {'Content-Type': 'application/json'});
+				res.end(JSON.stringify({
+					addedTutorial: result
+				}));
+			}
+		});
 });
 
 app.post('/api/get-all-tutorials', function (req, res) {
-	con.query("SELECT * FROM tutorials", function (err, result, fields) {
+	con.query("SELECT * FROM tutorials", function (err, result) {
 		if (err) {
 			console.log(err);
 		} else {
-			console.log(result);
+			console.log(result.length);
+			res.writeHead(200, {'Content-Type': 'application/json'});
+			res.end(JSON.stringify({
+				allTutorials: result.length
+			}));
 		}
 	});
 });
@@ -185,24 +190,25 @@ app.post('/api/get-tutorial', function (req, res) {
 });
 
 app.post('/api/delete-tutorial', function (req, res) {
-	var token = req.body.tokenId;
-	var tutorialID = req.body.tutorialID;
-	var userid = '';
+	const token = req.body.tokenId;
+	const tutorialID = req.body.tutorialID;
+	let userid = '';
 	client.verifyIdToken(
 		token,
 		'513403102947-k7qrh4q7s7p5ar7b1dpo27o768e6iq8i.apps.googleusercontent.com',
 		function (e, login) {
-			var payload = login.getPayload();
+			const payload = login.getPayload();
 			userid = payload['sub'];
 			// If request specified a G Suite domain:
 			//var domain = payload['hd'];
 		});
 	if (userid === morgan) {
-		var sql = "DELETE FROM tutorials WHERE id = '" + tutorialID + "'";
+		const sql = "DELETE FROM tutorials WHERE id = '" + tutorialID + "'";
 		con.query(sql, function (err, result) {
 			if (err) {
 				console.log(err);
 			} else {
+				console.log(result);
 				res.writeHead(200, {'Content-Type': 'application/json'});
 				res.end(JSON.stringify({
 					delete: 'Succesful: tutorial number ' + tutorialID + ' deleted.'
@@ -218,28 +224,39 @@ app.post('/api/delete-tutorial', function (req, res) {
 });
 
 app.post('/api/auth-user', function (req, res) {
-	var token = req.body.tokenId;
+	const token = req.body.tokenId;
+	let userid = '';
 	client.verifyIdToken(
 		token,
 		'513403102947-k7qrh4q7s7p5ar7b1dpo27o768e6iq8i.apps.googleusercontent.com',
 		function (e, login) {
-			var payload = login.getPayload();
-			var userid = payload['sub'];
-			// If request specified a G Suite domain:
-			//var domain = payload['hd'];
+			if (e) {
+				console.log(e);
+			} else {
+				const payload = login.getPayload();
+				userid = payload['sub'];
+				if (userid === morgan) {
+					res.end(JSON.stringify({
+						signIn: 'succesful',
+						admin: true
+					}));
+				} else {
+					res.writeHead(200, {'Content-Type': 'application/json'});
+					res.end(JSON.stringify({
+						signIn: 'succesful',
+						admin: false
+					}));
+				}
+			}
 		});
-	res.writeHead(200, {'Content-Type': 'application/json'});
-	res.end(JSON.stringify({
-		signIn: 'succesful'
-	}));
 });
-
+/* Utility
 app.get('/api/set-text', function (req, res) {
-	var sqlTitle = 'ALTER TABLE tutorials MODIFY title TEXT;';
-	var sqlLink = 'ALTER TABLE tutorials MODIFY link TEXT;';
-	var sqlDescription = 'ALTER TABLE tutorials MODIFY description TEXT;';
-	var sqlSummary = 'ALTER TABLE tutorials MODIFY summary TEXT;';
-	var sqlAuthor = 'ALTER TABLE tutorials MODIFY author TEXT;';
+	const sqlTitle = 'ALTER TABLE tutorials MODIFY title TEXT;';
+	const sqlLink = 'ALTER TABLE tutorials MODIFY link TEXT;';
+	const sqlDescription = 'ALTER TABLE tutorials MODIFY description TEXT;';
+	const sqlSummary = 'ALTER TABLE tutorials MODIFY summary TEXT;';
+	const sqlAuthor = 'ALTER TABLE tutorials MODIFY author TEXT;';
 
 	con.query(sqlTitle, function (err, result) {
 		if (err) {
@@ -247,28 +264,28 @@ app.get('/api/set-text', function (req, res) {
 		} else {
 			console.log('Set title as text');
 		}
-	})
+	});
 	con.query(sqlLink, function (err, result) {
 		if (err) {
 			console.log(err)
 		} else {
 			console.log('Set link as text');
 		}
-	})
+	});
 	con.query(sqlDescription, function (err, result) {
 		if (err) {
 			console.log(err)
 		} else {
 			console.log('Set desc as text');
 		}
-	})
+	});
 	con.query(sqlSummary, function (err, result) {
 		if (err) {
 			console.log(err)
 		} else {
 			console.log('Set summary as text');
 		}
-	})
+	});
 	con.query(sqlAuthor, function (err, result) {
 		if (err) {
 			console.log(err)
@@ -281,17 +298,17 @@ app.get('/api/set-text', function (req, res) {
 		text: 'succesful'
 	}));
 });
-
+*/
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {``
-	var err = new Error('Not Found');
+app.use(function (req, res, next) {
+	const err = new Error('Not Found');
 	err.status = 404;
 	next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res) {
 	// set locals, only providing error in development
 	res.locals.message = err.message;
 	res.locals.error = req.app.get('env') === 'development' ? err : {};
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,4 +1,4 @@
-var id_token;
+var idToken;
 function submitTutorial() {
     var title = $('#form-title').val();
     var desc = $('#tutorial-details').val();
@@ -18,11 +18,12 @@ function submitTutorial() {
         }
     }
     var submitUrl = "/api/add-tutorial?title=" + encodeURIComponent(title) + "&link=" + encodeURIComponent(url) + "&desc=" + encodeURIComponent(desc) + "&summary=" + encodeURIComponent(summary) + "&author=" + encodeURIComponent(username);
-    console.log(submitUrl)
+    console.log(submitUrl);
     $.ajax({
         method: 'post',
         url: submitUrl,
-        success: function(result1) {
+        success: function(result) {
+            console.log(result);
             window.location.href = 'submitted';
         },
         error: function(result, err) {
@@ -36,25 +37,32 @@ function recaptchaCallback() {
     $('#submitBtn').removeAttr('disabled');
     document.getElementById('submitBtn').setAttribute("class", "btn btn-primary");
     document.getElementById('submitBtn').style.cursor = 'pointer';
-};
+}
 
 function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    id_token = googleUser.getAuthResponse().id_token;
+    // noinspection JSUnresolvedFunction
+	var profile = googleUser.getBasicProfile();
+    // noinspection JSUnresolvedFunction
+	idToken = googleUser.getAuthResponse().id_token;
     $.ajax({
         method: 'post',
         url: '/api/auth-user',
         data: {
-            tokenId: id_token
+            tokenId: idToken
         },
         success: function (result) {
+            console.log(result);
             document.getElementById('g-signIn').style.display = 'none';
+			document.getElementById('g-signIn1').style.display = 'none';
             document.getElementById('logged-in').style.display = 'block';
+			document.getElementById('nav-logged-in').style.display = 'block';
             document.getElementById('new-tutorial').style.display = 'block';
             document.getElementById('login-dropdown').style.display = 'block';
             document.getElementById('new-tutorial').style.paddingLeft = '-8px';
             document.getElementById('login-dropdown').innerHTML = "Welcome, " + profile.getName() + '<i class="material-icons right">arrow_drop_down</i>';
-        },
+			document.getElementById('nav-welcome').innerHTML = "Welcome, " + profile.getName();
+
+		},
         error: function (err) {
             console.log(err);
         }
@@ -67,24 +75,18 @@ function onSignIn(googleUser) {
     */
 }
 function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
+	// noinspection JSUnresolvedFunction
+	/** @namespace gapi.auth2 */
+	var auth2 = gapi.auth2.getAuthInstance();
+    // noinspection JSUnresolvedFunction
+	auth2.signOut().then(function () {
         console.log('User signed out.');
     });
     document.getElementById('g-signIn').style.display = 'block';
+	document.getElementById('g-signIn1').style.display = 'none';
     document.getElementById('logged-in').style.display = 'none';
     document.getElementById('new-tutorial').style.display = 'none';
 
-}
-
-function getParameterByName(name, url) {
-    if (!url) url = window.location.href;
-    name = name.replace(/[\[\]]/g, "\\$&");
-    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
 
@@ -93,12 +95,14 @@ function search() {
         method: 'post',
         url: "/api/search-tutorials?q=" + getQueryString('q'),
         success: function(result1) {
-            var theSnippet = '<div class="row"><div class="col s12 m12"><div class="card z-depth-0" style="border-radius: 5px"><div class="card-content"> <a href="%LINK%"><h5 href="%LINK%" style="color: #0094ff; margin-top: 0px;">%TITLE%</h5></a> <p>%DESCRIPTION%</p></div></div></div></div>\n';
+            var theSnippet = '<div class="row"><div class="col s12 m12"><div class="card z-depth-0" style="border-radius: 5px"><div class="card-content"> <a href="%LINK%"><h5 style="color: #0094ff; margin-top: 0px;">%TITLE%</h5></a> <p>%DESCRIPTION%</p></div></div></div></div>\n';
             var results = result1.searchResults;
             document.getElementById('searchHeader').innerHTML = "Search - " + results.length + " results found";
             for (var i = 0; i < results.length; i++) {
                 var result = results[i];
-                document.getElementById('searchResults').innerHTML += theSnippet.replace(/%TITLE%/g, result.title).replace(/%DESCRIPTION%/g, new showdown.Converter().makeHtml(result.summary)).replace(/%LINK%/g, "tutorial?id=" + result.id);
+                $('#loading').remove();
+                // noinspection JSUnresolvedVariable
+				document.getElementById('searchResults').innerHTML += theSnippet.replace(/%TITLE%/g, result.title).replace(/%DESCRIPTION%/g, new showdown.Converter().makeHtml(result.summary)).replace(/%LINK%/g, "tutorial?id=" + result.id);
             }
         },
         error: function(result, err) {
@@ -110,7 +114,7 @@ function search() {
 
 function text() {
     $.ajax({
-        methpd: 'post',
+        method: 'post',
         url: '/api/set-text',
         success: function(result) {
             console.log(result)
@@ -129,7 +133,8 @@ function loadTutorial() {
             var tutorial = result1.searchResults[0];
             document.getElementById('tutorial-title').innerHTML = tutorial.title;
             document.getElementById('creator-name').innerHTML = tutorial.author;
-            document.getElementById('tutorial-desc').innerHTML = new showdown.Converter().makeHtml(tutorial.description);
+            // noinspection JSUnresolvedVariable
+			document.getElementById('tutorial-desc').innerHTML = new showdown.Converter().makeHtml(tutorial.description);
             document.getElementById('tutorial-link').outerHTML = "<a href='" + tutorial.link + "'>" + tutorial.link + "</a>";
         },
         error: function(result, err) {
@@ -151,7 +156,7 @@ function removeTutorial() {
         url: "/api/delete-tutorial",
         method: 'post',
         data: {
-            tokenId: id_token,
+            tokenId: idToken,
             tutorialID: getQueryString('id')
         },
         success: function(result){
